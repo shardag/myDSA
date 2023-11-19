5d86571ba1d5e3fe965d0f2d26d5977c6c0ecbd7
shardag <151423593+shardag@users.noreply.github.com>
2023-11-19 13:34:31 +0530

    Create N-Triangles.java

---
+++ patterns/N-Triangles.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// if n == 7
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 6 7 
