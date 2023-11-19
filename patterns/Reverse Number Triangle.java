503040f428386f9fd7ff48ca389312c8bcd4e730
shardag <151423593+shardag@users.noreply.github.com>
2023-11-19 13:37:43 +0530

    Create Reverse Number Triangle.java

---
+++ patterns/Reverse Number Triangle.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        // Write your code her
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// if n == 6
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
